5bc7d9e703613d3870313e7187ba8ccaf3f55f20
Adrián Garrido <adrian.galotecnia@gmail.com>
2019-02-04 00:47:27 +0000

    Added basic model

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,16 +1,15 @@
 <?php
 class Home extends CI_Controller {
-    public $static_url;
-
     public function __construct() {
         parent::__construct();
-        $this->static_url = base_url('static/');
+        $this->load->model('home');
     }
 
-    public function view()
-    {
+    public function view() {
         $data['title'] = 'Home'; // Capitalize the first letter
         $data["base_urls_list"] = $this->custom_commons->get_base_template_urls();
+        $data["geolocation_list"] = $this->home->get_fields('latitude, longitude');
+        var_dump($data["geolocation_list"]);
         echo $this->twig->render('pages/home', $data);
     }
 }
---
+++ application/models/Home.php
@@ -0,0 +1,24 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends CI_Model {
+
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function get_all() {
+        $query = $this->db->get('geolocation');
+        return $query->result_array();
+    }
+
+    public function get_fields($fields = '*') {
+        $this->db->select($fields);
+        $query = $this->db->get('geolocation');
+        return $query->result_array();
+    }
+}
+
+/* End of file Home.php */
